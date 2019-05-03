f4063cfaaddcbca9ab77b653b1ffe5d713f423e4
okchan08 <yoshiki.o1994@gmail.com>
2019-05-03 23:58:30 +0900

    Modify www page. Chart.js is added

--- sw/www/index.js
+++ sw/www/index.js
@@ -5,6 +5,53 @@ var DEFAULT_PORT = 7100;
 var ws = new WebSocket("ws://" + DEFAULT_DOMAIN + ":" + DEFAULT_PORT, "example-protocol");
 ws.binaryType = "arraybuffer";
 
+var canvas = null;
+var ctx = null;
+var chart = null;
+
+function init_canvas(){
+    canvas = document.getElementById('sample');
+    if(canvas.getContext){
+        ctx = canvas.getContext('2d');
+        chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: new Array(100),
+                datasets: [{
+                    label:'temp data',
+                    data: [],
+                }]
+            },
+                options: {
+                    elements: {
+                        line: {
+                            tension: 0
+                        }
+                    },
+                    scales: {
+                        xAxes: [{
+                            ticks:{
+                                beginAtZero: true,
+                                min: 0,
+                                max: 100
+                            }
+                        }],
+                        yAxes: [{
+                            ticks:{
+                                beginAtZero: true,
+                                min: 0,
+                                max: 12
+                            }
+                        }]
+                    }
+                } 
+            });
+        for(i=0;i<100;++i){
+            chart.data.datasets[0].data.push(0);
+        }
+    }
+}
+
 function stringOfUint8Array(u8){
     var ret = "";
     for(i=0;i<u8.length;++i){
@@ -36,19 +83,36 @@ function update_data(e_data){
     return [h8, h16, h32, moji];
 }
 
-function update_test(e_data){
-    var h8 = new Uint8Array(e_data, 0, 128);
-    var moji = stringOfUint8Array(h8);
-    return moji;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function copy_data(dst_data, length, src_data){
+    for(i=0;i<length;++i){
+        dst_data[i] = src_data[i];
+    }
+}
+
+function drawChart(){
+    var data_x = [];
+    var data_y = [];
+    var length = 100;
+    for(i=0;i<length;++i){
+        data_x.push(i);
+        //data_y.push(getRandomInt(0,10));
+        data_y.push(i);
+        chart.data.datasets[0].data[i] = getRandomInt(0,10);
+    }
+    chart.update();
 }
 
 ws.onopen = function() { console.log("socket open");};
 ws.onclose = function() { console.log("socket closed");};
 
 ws.onmessage = function(e){
-    //var[data0, data1, moji] = update_data(e.data);
-    //console.log("socket data:  " + data0 + "  :  " + data1 + "  :  " + moji);
-    //console.log("socket data: " + update_test(e.data));
+    drawChart();
     if(typeof e.data === "string"){
         console.log(e);
     } else {
